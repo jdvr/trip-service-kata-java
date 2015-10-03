fbffbfad3dbfb6c2b9049dff0a0d1ed79e752a03
Juan David Vega <jdvr.93@hotmail.com>
2015-10-03 10:23:23 +0200

    red: no logged in user

--- src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,5 +1,22 @@
 package org.craftedsw.tripservicekata.trip;
 
+import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
+import org.craftedsw.tripservicekata.user.User;
+import org.junit.Test;
+
 public class TripServiceTest {
+
+    /**
+     * TODO LIST:
+     *  No hay un usuario loggeado
+     *  El usuario logeado y el objetivono son amigos
+     *  El usuario logeado y el objetivo son amigo y el objetivo tiene viajes
+     */
+
+    @Test(expected = UserNotLoggedInException.class)
+    public void throw_an_exception_when_there_is_no_logged_user() throws Exception{
+        TripService tripService = new TripService();
+        tripService.getTripsByUser(new User());
+    }
 	
 }
